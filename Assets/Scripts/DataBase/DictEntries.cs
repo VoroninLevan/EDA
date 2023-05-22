d8cf815cde22b16e6003d66928e9ca16a4294845
Levan <voroninlevan@gmail.com>
2023-05-23 00:55:28 +0100

    Static db entries

---
+++ Assets/Scripts/DataBase/DictEntries.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictEntries
+{
+    public static readonly string Merc_Info1 = "From the surface of Mercury, the Sun would appear more than three times as large as it does when viewed from Earth, and the sunlight would be as much as 11 times brighter. The smallest planet in our solar system and nearest to the Sun, Mercury is only slightly larger than Earths Moon.";
+    public static readonly string Merc_Count = "1";
+    
+    public static readonly string Ven_Info1 = "Similar in structure and size to Earth, Venus's thick atmosphere traps heat in a runaway greenhouse effect, making it the hottest planet in our solar system. Venus is the second planet from the Sun and is Earth’s closest planetary neighbor. It’s one of the four inner, terrestrial (or rocky) planets, and it’s often called Earth’s twin because it’s similar in size and density. These are not identical twins, however – there are radical differences between the two worlds.";
+    public static readonly string Ven_Count = "1";
+    
+    public static readonly string Earth_Info1 = "Earth—our home planet—is the only place we know of so far that’s inhabited by living things. Its also the only planet in our solar system with liquid water on the surface. Our home planet is the third planet from the Sun, and the only place we know of so far that’s inhabited by living things.";
+    public static readonly string Earth_Count = "1";
+    
+    public static readonly string Mars_Info1 = "Mars is a dusty, cold, desert world with a very thin atmosphere. There is strong evidence Mars was – billions of years ago – wetter and warmer, with a thicker atmosphere. Mars is the fourth planet from the Sun – a dusty, cold, desert world with a very thin atmosphere. Mars is also a dynamic planet with seasons, polar ice caps, canyons, extinct volcanoes, and evidence that it was even more active in the past.";
+    public static readonly string Mars_Count = "1";
+    
+    public static readonly string Jupiter_Info1 = "Jupiter is more than twice as massive than the other planets of our solar system combined. The giant planets Great Red Spot is a centuries-old storm bigger than Earth. Jupiter has a long history of surprising scientists – all the way back to 1610 when Galileo Galilei found the first moons beyond Earth. That discovery changed the way we see the universe.";
+    public static readonly string Jupiter_Count = "1";
+    
+    public static readonly string Sat_Info1 = "Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system. The other giant planets have rings, but none are as spectacular as Saturn's. Saturn is the sixth planet from the Sun and the second-largest planet in our solar system.";
+    public static readonly string Sat_Count = "1";
+    
+    public static readonly string Ura_Info1 = "Uranus—seventh planet from the Sun—rotates at a nearly 90-degree angle from the plane of its orbit. This unique tilt makes Uranus appear to spin on its side. Uranus is the seventh planet from the Sun, and has the third-largest diameter in our solar system. It was the first planet found with the aid of a telescope, Uranus was discovered in 1781 by astronomer William Herschel, although he originally thought it was either a comet or a star.";
+    public static readonly string Ura_Count = "1";
+    
+    public static readonly string Nep_Info1 = "Neptune—the eighth and most distant major planet orbiting our Sun—is dark, cold and whipped by supersonic winds. It was the first planet located through mathematical calculations. Dark, cold, and whipped by supersonic winds, ice giant Neptune is the eighth and most distant planet in our solar system.";
+    public static readonly string Nep_Count = "1";
+    
+    public static readonly string Moo_Info1 = "The fifth largest moon in the solar system, Earths moon is the only place beyond Earth where humans have set foot.";
+    public static readonly string Moo_Info2 = "The brightest and largest object in our night sky, the Moon makes Earth a more livable planet by moderating our home planets wobble on its axis, leading to a relatively stable climate.";
+    public static readonly string Moo_Info3 = "Earth's Moon is the fifth largest of the 200+ moons orbiting planets in our solar system.";
+    public static readonly string Moo_Count = "3";
+    
+}
