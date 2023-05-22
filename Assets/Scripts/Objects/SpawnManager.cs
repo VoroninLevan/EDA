24d4c02c354aaf134f8b591b60bee028631ba68f
Levan <voroninlevan@gmail.com>
2023-05-23 00:53:10 +0100

    Maintenance

--- Assets/Scripts/Objects/SpawnManager.cs
+++ Assets/Scripts/Objects/SpawnManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.XR.ARFoundation;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -13,16 +12,5 @@ public class SpawnManager : MonoBehaviour
     private void Start()
     {
         if(gameObject.activeSelf) transform.rotation = Quaternion.Euler(0, 0, 0);
-    }
-
-    public void SetSpawnedObject(GameObject spawnedObject)
-    {
-        _currentlySpawned = spawnedObject;
-    }
-
-    public void DestroySpawnedObject()
-    {
-        GameObject.Destroy(_currentlySpawned);
-        _currentlySpawned = null;
     }
 }
