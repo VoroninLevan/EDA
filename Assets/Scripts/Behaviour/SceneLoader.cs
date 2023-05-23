14af02bc0a7ced2a8bc5697a29f4f4cbce517a61
Levan <voroninlevan@gmail.com>
2023-05-23 11:38:52 +0100

    Scene loader implementation

--- Assets/Scripts/Behaviour/SceneLoader.cs
+++ Assets/Scripts/Behaviour/SceneLoader.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.XR.ARFoundation;
 
 public class SceneLoader : MonoBehaviour
 {
